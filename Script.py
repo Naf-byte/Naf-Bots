e31ff96b0189c1fc3e3111046b4aa8a8b2e4328f
Nafay Ur Rehman <131803895+Naf-byte@users.noreply.github.com>
2024-06-22 22:43:28 +0500

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,62 +1,274 @@
+import collections.abc
+# try:
+#     from collections.abc import Iterable
+# except ImportError:
+#     from collections import Iterable
+from collections.abc import Iterable
+# Manually create aliases for the classes expected by the third-party library
+collections.Iterable = collections.abc.Iterable
+collections.Mapping = collections.abc.Mapping
+collections.MutableSet = collections.abc.MutableSet
+collections.MutableMapping = collections.abc.MutableMapping
+# Now import the third-party library that was causing the issue
+import hyper
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain_community.embeddings import GooglePalmEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain_community.llms import GooglePalm
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import GooglePalmEmbeddings
+from langchain.llms import GooglePalm
+from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
+import os
+import time
 
+
+os.environ['GOOGLE_API_KEY'] = 'AIzaSyBZP1trD9aesCfXyiV5vQClnbKsVXlM89s'
+
+
+# Function to extract text from PDF files
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
-        reader = PdfReader(pdf)
-        for page in reader.pages:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
             text += page.extract_text()
     return text
 
-def get_text_chunks(text):
-    # Placeholder implementation for splitting text into chunks
-    chunk_size = 500  # Adjust as needed
-    text_chunks = [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
-    return text_chunks
 
+# Function to split text into chunks
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+# Function to create the vector store
 def get_vector_store(text_chunks):
     embeddings = GooglePalmEmbeddings()
-    text_embeddings = [embeddings.embed(text_chunk) for text_chunk in text_chunks]
-    
-    # Debugging statements
-    print(f"Number of text chunks: {len(text_chunks)}")
-    print(f"Number of embeddings: {len(text_embeddings)}")
-    print(f"Embedding size: {len(text_embeddings[0]) if text_embeddings else 'N/A'}")
-    
-    vector_store = FAISS.from_texts(text_chunks, embedding=text_embeddings)
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     return vector_store
 
+
+# Function to create the conversational chain
 def get_conversational_chain(vector_store):
     llm = GooglePalm()
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(),
+                                                               memory=memory)
     return conversation_chain
 
-st.title("PDF Conversational AI")
-st.subheader("Upload your PDF files and start a conversation")
+# Function to handle user input and display chat messages
+def user_input(user_question):
+    conversation = st.session_state.conversation
 
-uploaded_files = st.file_uploader("Upload PDF", type="pdf", accept_multiple_files=True)
-if st.button("Process"):
-    if uploaded_files:
-        raw_text = get_pdf_text(uploaded_files)
-        text_chunks = get_text_chunks(raw_text)
-        vector_store = get_vector_store(text_chunks)
-        st.session_state.conversation = get_conversational_chain(vector_store)
-        st.success("Processing Completed!")
-    else:
-        st.error("Please upload PDF files to proceed.")
+    if conversation is None:
+        st.error("""
+        Conversation chain not initialized!! \n
+        Please upload PDF files first by clicking arrow button on the Top left corner
+        """)
+        return
+        
+    with st.spinner("Generating response..."):
+        time.sleep(1)  # Reduce spinner time to 1 second
+        response = st.session_state.conversation({'question': user_question})
+        st.session_state.chatHistory = response['chat_history']
 
-if 'conversation' in st.session_state:
-    user_input = st.text_input("Your Question:")
-    if user_input:
-        response = st.session_state.conversation.ask(user_input)
-        st.write(response)
+        # Reverse the order of chat history to display recent messages on top
+        st.session_state.chatHistory.reverse()
 
-if st.button("Cancel"):
+        for i, message in enumerate(st.session_state.chatHistory):
+            if i % 2 == 0:
+                st.markdown(
+                    f"<div style='background-color: #32CD32; color: black; padding: 10px; border-radius: 10px; margin: 10px;'>"
+                    f" [Me]🤖 :  {message.content}"
+                    "</div>",
+                    unsafe_allow_html=True
+                )
+            else:
+                user_response = message.content
+                st.markdown(
+                    f"<div style='background-color: #00008B; color: white; padding: 10px; border-radius: 10px; margin: 10px;'>"
+                    f" [You]👱 :  {user_response}"
+                    "</div>",
+                    unsafe_allow_html=True
+                )
+
+
+# Function to create the conversational chain
+def get_conversational_chain(vector_store):
+    llm = GooglePalm()
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(),
+                                                               memory=memory)
+    return conversation_chain
+
+
+# Set page config and title
+st.set_page_config(page_title="Naf-Chat", page_icon=":speech_balloon:")
+
+# Sidebar CSS styling
+st.sidebar_css = """
+    <style>
+        .sidebar .sidebar-content {
+            background-color: #2c3e50;
+            color: white;
+            padding: 20px;
+            border-radius: 10px;
+        }
+        .sidebar .sidebar-content .stButton>button {
+            background-color: #2980b9 !important;
+            color: white !important;
+            border-radius: 5px;
+        }
+    </style>
+"""
+st.markdown(st.sidebar_css, unsafe_allow_html=True)
+
+# Introduction popup with animated bot and title
+st.write(
+    """
+    <div style='text-align:center;'>
+        <div style="display: flex; justify-content: space-between; align-items: center;">
+            <h1>Naf-Chat: Chat with Multiple PDF Files </h1>
+        </div>
+        <h2>Welcome to Naf-Chat!</h2>
+        <p>Ask me anything about your PDF files, and I'll do my best to help you out.</p>
+        <p>I'm a chatbot created by Nafay Ur Rehman. My purpose is to assist you with your PDF-related queries.</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+# Initialize conversation and chat history
+if "conversation" not in st.session_state:
     st.session_state.conversation = None
+if "chatHistory" not in st.session_state:
+    st.session_state.chatHistory = None
+
+# Sidebar for settings
+with st.sidebar:
+    st.title("Settings")
+    st.subheader("Upload your Documents")
+    pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Upload Button", accept_multiple_files=True)
+    col1, col2 = st.columns(2)
+    if col1.button("Upload"):
+        with st.spinner("Processing..."):
+            raw_text = get_pdf_text(pdf_docs)
+            text_chunks = get_text_chunks(raw_text)
+            vector_store = get_vector_store(text_chunks)
+            st.session_state.conversation = get_conversational_chain(vector_store)
+            st.success("Processing Completed!!<")
+
+    if col2.button("Cancel"):
+        st.warning("Upload canceled")
+
+# Display user input field
+user_question = st.text_input("Ask a Question from the PDF Files")
+if user_question:
+    user_input(user_question)
+
+
+# Function to handle user input and display chat messages
+# def user_input(user_question):
+#     if user_question.lower() == "who is nafay" or "who is nafay ur rehman" or "who is nafay?" or "who is nafay ur rehman?":
+#         st.markdown(
+#             f"<div style='background-color: #32CD32; color: black; padding: 10px; border-radius: 10px; margin: 10px;'>"
+#             f" **{{Naf-Chat}}** :  He is the Founder of me, He used different Generative AI techniques to create me"
+#             f" **{{Naf-Chat}}** :  Why are you asking me?"
+#             "</div>",
+#             unsafe_allow_html=True
+#         )
+#         return
+
+#     with st.spinner("Generating response..."):
+#         time.sleep(1)  # Reduce spinner time to 1 second
+#         response = st.session_state.conversation({'question': user_question})
+#         st.session_state.chatHistory = response['chat_history']
+
+#         # Reverse the order of chat history to display recent messages on top
+#         st.session_state.chatHistory.reverse()
+
+#         for i, message in enumerate(st.session_state.chatHistory):
+#             if i % 2 == 0:
+#                 st.markdown(
+#                     f"<div style='background-color: #32CD32; color: black; padding: 10px; border-radius: 10px; margin: 10px;'>"
+#                     f" **{{Naf-Chat}}** :  {message.content}"
+#                     "</div>",
+#                     unsafe_allow_html=True
+#                 )
+#             else:
+#                 user_response = message.content
+#                 st.markdown(
+#                     f"<div style='background-color: #00008B; color: white; padding: 10px; border-radius: 10px; margin: 10px;'>"
+#                     f" **{{You}}** :  {user_response}"
+#                     "</div>",
+#                     unsafe_allow_html=True
+#                 )
+
+
+# # Set page config and title
+# st.set_page_config(page_title="Naf-Chat", page_icon=":speech_balloon:")
+
+# # Sidebar CSS styling
+# st.sidebar_css = """
+#     <style>
+#         .sidebar .sidebar-content {
+#             background-color: #2c3e50;
+#             color: white;
+#             padding: 20px;
+#             border-radius: 10px;
+#         }
+#         .sidebar .sidebar-content .stButton>button {
+#             background-color: #2980b9 !important;
+#             color: white !important;
+#             border-radius: 5px;
+#         }
+#     </style>
+# """
+# st.markdown(st.sidebar_css, unsafe_allow_html=True)
+
+# # Introduction popup with animated bot and title
+# st.write(
+#     """
+#     <div style='text-align:center;'>
+#         <div style="display: flex; justify-content: space-between; align-items: center;">
+#             <img src='https://cdn.dribbble.com/userupload/10543014/file/original-4703d0ba72b72f87fa49a618a24a1f6d.gif' alt='Naf-Chat Bot' style='width: 100px; height: 100px; border-radius: 50%; margin-right: 10px;'/>
+#             <h1>Naf-Chat: Chat with Multiple PDF Files </h1>
+#             <img src='https://cdn.dribbble.com/userupload/10543014/file/original-4703d0ba72b72f87fa49a618a24a1f6d.gif' alt='Naf-Chat Bot' style='width: 100px; height: 100px; border-radius: 50%; margin-left: 10px;'/>
+#         </div>
+#         <h2>Welcome to Naf-Chat!</h2>
+#         <p>Ask me anything about your PDF files, and I'll do my best to help you out.</p>
+#         <p>I'm a chatbot created by Nafay Ur Rehman. My purpose is to assist you with your PDF-related queries.</p>
+#     </div>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+# # Initialize conversation and chat history
+# if "conversation" not in st.session_state:
+#     st.session_state.conversation = None
+# if "chatHistory" not in st.session_state:
+#     st.session_state.chatHistory = None
+
+# # Sidebar for settings
+# with st.sidebar:
+#     st.title("Settings")
+#     st.subheader("Upload your Documents")
+#     pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Upload Button", accept_multiple_files=True)
+#     col1, col2 = st.columns(2)
+#     if col1.button("Upload"):
+#         with st.spinner("Processing..."):
+#             raw_text = get_pdf_text(pdf_docs)
+#             text_chunks = get_text_chunks(raw_text)
+#             vector_store = get_vector_store(text_chunks)
+#             st.session_state.conversation = get_conversational_chain(vector_store)
+#             st.success("Processing Completed!!<")
+
+#     if col2.button("Cancel"):
+#         st.warning("Upload canceled")
+
+# # Display user input field
+# user_question = st.text_input("Ask a Question from the PDF Files")
+# if user_question:
+#     user_input(user_question)
